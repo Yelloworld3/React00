86a991d819903b391d463a8ef7ceb300cadc16af
Yelloworld3 <157587261+Yelloworld3@users.noreply.github.com>
2025-07-14 19:33:32 +0330

    update00
    training some new features.

--- Teo17.jsx
+++ Teo17.jsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client"
 
 const root = createRoot(document.getElementById("root"))
 
-root.render(<p>This is a test and the first step. Hello reaact!</p>)
+    root.render(
+
+    createElement("h1", null, "Hello from createElement!")
+
+)
